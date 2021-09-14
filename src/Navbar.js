16067c3bb2a334fa1de30575bbb3703df55baca1
gshr <gaurav.sharma1054@gmail.com>
2021-09-14 16:01:55 +0530

    Adding Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,14 @@
+import './Navbar.css'
 function Navbar(){
 
 return (
-    <div>
+    <div className="nav-bar">
+      <ul className ="ul">
+        <li >  <a class ="active" href="/">Home</a></li>
+        <li className="About"> <a href="/">About</a></li>
+        <li> <a href="/">Projects</a></li>
+        <li> <a href="/">Contact</a></li>
+      </ul>
    
   </div>
 );
